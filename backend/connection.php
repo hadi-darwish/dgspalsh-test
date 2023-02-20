a8491352e91a1a3e04cd1cf808dd1d9cc3ee82a5
hadi-darwish <hadi.darwish.03@gmail.com>
2023-02-20 12:25:35 +0200

    adding database connection class

--- backend/connection.php
+++ backend/connection.php
@@ -1,11 +1,17 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-$host = "localhost";
-$db_user = "root";
-$db_pass = "4423";
-$db_name = "dgsplashdb";
+class DataBaseConnection
+{
+    protected $host = "localhost";
+    protected $db_user = "root";
+    protected $db_pass = "4423";
+    protected $db_name = "dgsplashdb";
 
-
-$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+    public function connect()
+    {
+        $connection = new mysqli($this->host, $this->db_user, $this->db_pass, $this->db_name);
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        return $connection;
+    }
+}
