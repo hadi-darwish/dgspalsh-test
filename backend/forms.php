e22f9dfa91dd1c1ec824e4b5a1468d3be7b62725
hadi-darwish <hadi.darwish.03@gmail.com>
2023-02-20 14:22:28 +0200

    adding setters

--- backend/forms.php
+++ backend/forms.php
@@ -27,4 +27,49 @@ class Forms
     {
         $this->database = new Database();
     }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+    }
+
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function setCreatedOn($createdOn)
+    {
+        $this->createdOn = $createdOn;
+    }
 }
