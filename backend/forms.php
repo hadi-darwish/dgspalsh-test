55b012a7d25a7dfd76a5e847051694950b628740
hadi-darwish <hadi.darwish.03@gmail.com>
2023-02-22 17:56:24 +0200

    removing unnecessary headers

--- backend/forms.php
+++ backend/forms.php
@@ -1,10 +1,4 @@
 <?php
-
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-
 include('database.php');
 
 class Forms
